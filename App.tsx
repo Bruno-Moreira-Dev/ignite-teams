009d47ce06521f9acdb7d394c7c751b4d4ed0ab4
Bruno Pavan Adario Moreira <bpmoreira@TOPAZ.CORP>
2024-09-09 17:22:03 -0300

    feat(ignite-teams): criacao das telas do ignite-teams

--- App.tsx
+++ App.tsx
@@ -6,8 +6,8 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
 import { StatusBar } from "react-native";
-import theme from "src/theme";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
