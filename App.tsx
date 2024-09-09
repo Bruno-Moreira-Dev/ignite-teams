5dd194491e00b64ea367006ea5bb80fb1a0c55a2
Bruno Pavan Adario Moreira <bpmoreira@TOPAZ.CORP>
2024-09-09 11:58:52 -0300

    feat(ignite-teams): criacao do esqueleto da tela principal

--- App.tsx
+++ App.tsx
@@ -1,5 +1,25 @@
+import { Loading } from "@components/Loading";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
+import { StatusBar } from "react-native";
+import theme from "src/theme";
+import { ThemeProvider } from "styled-components";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
